b28a96309f7e76bef789a7504ce6b7209df2a7e7
Sam <sdbernhardt@gmail.com>
2026-02-01 17:31:16 -0500

    Move UIFork import

--- apps/sandbox/src/main.tsx
+++ apps/sandbox/src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "./components/theme-provider";
 import "./index.css";
+import { UIFork } from "uifork";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <UIFork />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
